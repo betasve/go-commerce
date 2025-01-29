972b2578d5fe56ff78ec1f203677dc29e57a9751
Svetoslav Blyahov <svetlio.blyahoff@gmail.com>
2025-01-29 22:08:21 +0200

    Add validators

--- services/auth/cmd/api/users.go
+++ services/auth/cmd/api/users.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/betasve/go-commerce/services/auth/internal/data"
+	"github.com/betasve/go-commerce/services/auth/internal/validator"
 )
 
 func (app *application) createUserHandler(w http.ResponseWriter, r *http.Request) {
@@ -18,6 +19,38 @@ func (app *application) createUserHandler(w http.ResponseWriter, r *http.Request
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+
+		return
+	}
+
+	v := validator.New()
+
+	v.Check(input.Name != "", "name", "can't be blank")
+	v.Check(len(input.Name) > 5, "name", "can't be less than 5 characters")
+	v.Check(input.Email != "", "email", "can't be blank")
+	// TODO: Add requirements for stronger password
+	v.Check(input.Password != "", "password", "can't be blank")
+
+	v.Check(
+		validator.Matches(
+			input.Name,
+			validator.NameRX,
+		),
+		"name",
+		"does not look like a valid name",
+	)
+
+	v.Check(
+		validator.Matches(
+			input.Email,
+			validator.EmailRX,
+		),
+		"email",
+		"does not look like a valid email",
+	)
+
+	if v.Invalid() {
+		app.failedValidationResponse(w, r, v.Errors)
 
 		return
 	}
