6328dc30ccb5118bfb347decf22bdefd1066eefe
Svetoslav Blyahov <svetlio.blyahoff@gmail.com>
2025-02-19 10:36:14 +0200

    create db connection

--- services/auth/cmd/api/main.go
+++ services/auth/cmd/api/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -14,6 +21,10 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn     string
+		migrate string
+	}
 }
 
 type application struct {
@@ -26,9 +37,21 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "development|staging|production")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GO_COMMERCE_DB_DSN"), "PostgreSQL DSN")
+	flag.StringVar(&cfg.db.migrate, "db-migrate", "false", "Trigger DB Migration")
+
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	defer db.Close()
+
+	logger.Printf("database connection pool established")
 
 	app := application{
 		config: cfg,
@@ -43,7 +66,61 @@ func main() {
 		WriteTimeout: 30 * time.Second,
 	}
 
+	if cfg.db.migrate == "true" {
+		err := migrateDB(db)
+
+		if err != nil {
+			logger.Fatal(err)
+		} else {
+			os.Exit(0)
+		}
+	}
+
 	logger.Printf("starting %s server on %d", cfg.env, cfg.port)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func migrateDB(db *sql.DB) error {
+	migrationDriver, err := postgres.WithInstance(db, &postgres.Config{})
+
+	if err != nil {
+		return err
+	}
+
+	migrator, err := migrate.NewWithDatabaseInstance(
+		fmt.Sprintf("file:///%s/db/migrations/", os.Getenv("SERVICE_ROOT_PATH")),
+		"postgres",
+		migrationDriver,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	err = migrator.Up()
+
+	if err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+
+	return nil
 }
