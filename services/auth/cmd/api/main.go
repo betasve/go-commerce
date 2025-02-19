a5e66bbca1e0126b4e3c95562c1432f842500883
Svetoslav Blyahov <svetlio.blyahoff@gmail.com>
2025-02-19 12:37:58 +0200

    add users model

--- services/auth/cmd/api/main.go
+++ services/auth/cmd/api/main.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/betasve/go-commerce/services/auth/internal/data"
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
@@ -30,6 +31,7 @@ type config struct {
 type application struct {
 	config config
 	logger *log.Logger
+	models data.Models
 }
 
 func main() {
@@ -56,6 +58,7 @@ func main() {
 	app := application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	srv := &http.Server{
---
+++ services/auth/internal/data/models.go
@@ -0,0 +1,31 @@
+package data
+
+import (
+	"database/sql"
+	"errors"
+)
+
+var (
+	ErrRecordNotFound = errors.New("record not found")
+)
+
+type Models struct {
+	Users interface {
+		Insert(user *User) error
+		Get(id int64) (*User, error)
+		Update(user *User) error
+		Delete(id int64) error
+	}
+}
+
+func NewModels(db *sql.DB) Models {
+	return Models{
+		Users: UserModel{DB: db},
+	}
+}
+
+func NewMockModels() Models {
+	return Models{
+		Users: MockUserModel{},
+	}
+}
